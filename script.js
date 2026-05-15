2b861bd0dce0be52e8e4c8866ba4d220fd22e12b
NAVEENKUMAR2003301 <livnaveen@gmail.com>
2026-05-15 17:30:27 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,209 @@
+// // javaScript
+
+// // scripting language
+
+// // 1. kjbhcjdbjksbkjbjchsbjkcbjhsdbjcbjsh
+// // 2. jbhjsbdjhbchjsdbvhjbfsdvhbkdjvhbv
+// // 3. jhshfhbsbhhufbsui
+
+
+// // variables 
+
+// // var  - global scope & function scope
+
+// // let  - block scope
+
+// // const - block scope
+
+
+// // rules : - 
+
+// // var rules 
+
+
+// var a = 20                // declration   // intialiazation
+
+//     a = 25               // reuse        // reinitialization
+
+// var a = 30                // reDeclration
+
+// console.log(a)
+
+
+// // let rules 
+
+// let b = 40
+
+//     b = 45
+
+// // let b = 50        // not accept redeclration
+
+// console.log(b);
+
+// // const rules - constant
+
+
+// const c = 60
+
+//     //   c = 65    not reuse , reintialization 
+
+// // const c = 70    not accept redeclration
+
+// console.log(c);
+
+
+
+// Examples :- 
+
+
+// 1. What is the output of the below code ?
+
+var a = 10
+a = 15
+var a = 20
+
+console.log(a) // 20
+
+
+// 2. Which keyword allows redeclaration ?
+
+// var
+
+
+// 3. Which keyword is used for constant values ?
+
+// constant
+
+
+// 4. Find the error in the below code
+
+let b = 30
+// let b = 40
+
+console.log(b) // syntex error
+
+
+// 5. What is reinitialization ? 
+
+//  
+
+
+// 6. What is the difference between var and let ?
+
+
+// 7. What is the output ?
+
+const c = 50
+
+console.log(c) // 50
+
+
+// 8. Can we reassign value to const ? 
+
+// 
+
+
+// 9. Which keyword does not allow redeclaration but allows reinitialization ?
+
+
+// 10. Write one example for:
+// a) var
+
+var a1 = 15
+
+    a1 = 20
+
+var a1 = 27
+
+console.log(a1);
+
+
+// b) let 
+
+let a2 = 20
+
+    a2 = 25
+
+// let a2 
+
+console.log(a2);
+
+
+
+// c) const 
+
+const a3 = 27
+
+    //   a3 = 50
+
+// const a3 
+
+console.log(a3);
+
+
+
+
+// printing Statement 
+
+
+// 1. console.log()
+
+console.log(100);
+
+let a5 = 123
+
+console.log(a5);
+
+
+
+// 2. alert()
+
+// alert(100)
+
+// let a6 = 1234
+
+// alert(a6)
+
+// 3. confirm()
+
+// confirm("did you like program")
+
+// 4. prompt()
+
+// prompt("what is your name")
+
+// 5. document.writeln()
+
+// document.writeln("sdhbjshbjcsd")
+
+
+// example :- 
+
+// 1. i want to print value "hello" in console side
+
+console.log("hello");
+
+
+// 2. i want to show "hello" to user the way is pop up
+
+// alert("hello")
+
+// 3. i want to print user age to console side
+
+// let userAge = prompt("enter your age")
+
+// console.log(userAge);
+
+
+// 4. i want to know user is complete html and css
+
+// let userCompletion = confirm("did you complete html,css")
+
+// console.log(userCompletion);
+
+
+
+// 5. i want to print in ui "batch 33"
+
+document.writeln("batch 33")
+
